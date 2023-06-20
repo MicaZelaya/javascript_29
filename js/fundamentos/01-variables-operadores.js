b5fa382f5902038da066819beea12dea2d1a1556
Micaela Zelaya <mika_z@live.com.ar>
2023-06-20 01:35:59 -0300

    Variables y operadores en Javascript

---
+++ js/fundamentos/01-variables-operadores.js
@@ -0,0 +1,81 @@
+//alert("Bienvenidos a Javascript");
+console.log("Bienvenidos a Javascript!!")
+console.log(5+3);
+
+//mostrar en el navegador
+document.write("Es un gusto saludarte");
+
+//variables: son espacios en memoria que usamos para guardar datos
+
+// declara: se las crea
+//inicializa: asignar un valor 
+
+var nombre;
+nombre = "Pepito";
+
+let apellido ="Gonzalez";
+
+const pi = 3.14;
+
+//tipos de datos
+//String: son de textos
+let cliente="Fabrizio"
+let alumno="Roberto Gonzalez"
+let mensaje="Soy feliz"
+
+//number
+let numero1=34
+let numero2=-52
+let numer3=58.5
+
+//boolean: verdadero o falso
+let comprar = true
+let comer = false
+
+//null
+let billetera = null
+
+//undefined: declaro una variable y no le asigno valor
+let caja;
+
+//array: arreglos, matrices
+let arreglo = ["Pablo",45,false]
+let alumnos = ["Jose","Alberto","Camila"]
+
+//object : cada propiedad tiene un valor
+let persona = {
+    nombre:"Josefina",
+    apellido:"Naranjo",
+    edad:33,
+}
+
+//operadores
+ //aritmeticos
+ let num1 = 20;
+ let num2 = 2;
+
+ let suma = numero1 + numero2;
+ let resta = numero1 - numero2;
+ let multiplicacion = numero1 * numero3;
+ let division = numero1 / numero2;
+ let resto = numero1 % numero2;
+
+ //unitarios
+ ++num2
+ --num2
+
+ //relacionales
+ num1="2"
+ num2="22"
+ console.log(numero1 == numero2);
+ console.log(num1 >= num2);
+ console.log(numero <=numero2);
+ console.log(numero1 != numero2);
+
+ console.log("El resultado de la suma es" + suma);
+ console.log("El resultado de la resta es ", resta);
+
+//negacion
+let activado = true;
+console.log(!activado);
+
